6cb1efce8ad33d29ff7b146f3e204f8c12be8b25
Stas Mikhel <mikhel.sk@gmail.com>
2019-02-19 20:38:58 +0300

    Add in-place painting, spatial rotaion, update alphabet

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from alphabet import alphabet
 import sys
+import math
 
 
-def convert(msgs, fontx=20, fonty=20, spacex=3, spacey=8, up=5):
+def convert(msgs, fontx=20, fonty=20, spacex=3, spacey=8, up=5, shift=True):
     x = 0
     y = 0
     z = 1 * up
@@ -13,44 +14,77 @@ def convert(msgs, fontx=20, fonty=20, spacex=3, spacey=8, up=5):
         msg = msg.upper()
         x = 0
         for character in msg:
-            if character == " ":
-                x += fontx
-                continue
             if character in alphabet:
                 letter = alphabet[character]
                 for dot in letter:
                     res.append((x + dot[0] * fontx, y + dot[1] * fonty, dot[2] * up))
-                x += fontx
+                if shift:
+                    x += fontx
             else:
                 print("not in the alphabet: ", character)
-
-            x += spacex
-        y -= fonty + spacey
+            if character == " " and shift:
+                x += fontx
+            if shift:
+                x += spacex
+        if shift:
+            y -= fonty + spacey
     return res
+    
+def matZYX(rx,ry,rz):
+    ca, cb, cc = math.cos(rz), math.cos(ry), math.cos(rx)
+    sa, sb, sc = math.sin(rz), math.sin(ry), math.sin(rx)
+    # rotation matrix Z-Y-X	
+    return [[ca*cb, ca*sb*sc-sa*cc, sa*sc+ca*sb*cc],
+            [sa*cb, sa*sb*sc+ca*cc, sa*sb*cc-ca*sc],
+            [  -sb,          cb*sc,          cb*cc]]
 
+# points : get after conversation
+# translation : [dx,dy,dz]
+# rotation : [rotZ,rotY,rotX]            
+def transform(points, translation, rotation):
+    R = matZYX(rotation[0],rotation[1],rotation[2])
+    res = []
+    for c in points:
+        x = R[0][0]*c[0]+R[0][1]*c[1]+R[0][2]*c[2]+translation[0]
+        y = R[1][0]*c[0]+R[1][1]*c[1]+R[1][2]*c[2]+translation[1]
+        z = R[2][0]*c[0]+R[2][1]*c[1]+R[2][2]*c[2]+translation[2]
+        res.append([x,y,z,rotation[0],rotation[1],rotation[2]])
+    return res        
 
 def main():
-    # msgs = ["Hello world!", u"Russian: Привет мир", "subscribe!"]
+    #msgs = ["Hello world!", u"Russian: Привет мир", "subscribe!"]
+        
+#    msgs = [u"Я был бы несчастливейшим",
+#           u"из людей,ежели бы я",
+#           u"не нашел цели для моей",
+#           u"жизни - цели общей и",
+#           u"полезной. Теперь же жизнь",
+#           u"моя будет вся стремлением",
+#           u"деятельным и постоянным",
+#           u"к этой одной цели.",
+#           u"           Л.Н.Толстой"]
 
-    msgs = [u"Я был бы несчастливейшим",
-            u"из людей, ежели бы я",
-            u"не нашел цели для моей",
-            u"жизни - цели общей и полезной.",
-            u"Теперь же жизнь моя будет вся",
-            u"стремлением деятельным и",
-            u"постоянным к этой одной цели."]
-
-    # msgs = [u"Один,", u"Два"]
+    #msgs = [u"Один,", u"Два"]
+    
+    msgs = ["INNOPOLIS"]
 
     if len(sys.argv) > 1:
         msg = " ".join(sys.argv[1:])
         msgs = [msg]
+        
+    FONTY = 200
+    FONTX = 300
 
-    dots = convert(msgs, fontx=10, fonty=10, spacey=4)
+    dots = convert(msgs,fontx=300,fonty=FONTY,up=-50,shift=False)
+    # rotate in xy plane (around Z axis)
+    dots = transform(dots, [0,0,0], [0,0,0.5*math.pi])
+    # spacial transformation
+    dots = transform(dots, [650,-0.5*FONTY,650-0.5*FONTX], [math.pi,math.pi*0.5,math.pi])
 
     with open("output.csv", 'w') as f:
-        for x, y, z in dots:
-            f.write("{:.5},{:.5},{:.5}\n".format(float(x), float(y), float(z)))
+        for d in dots:
+            f.write(",".join(["{:.5}".format(float(i)) for i in d]))
+            f.write("\n")
 
     print("Result has been printed to output.csv. Done!")
 
--- turtle_sim.py
+++ turtle_sim.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 import turtle
 import time
-from main import convert
+from main import convert, transform
+import math
 import sys
 
 
 def main():
-    # msgs = ["Hello world!", u"Russian: Привет мир", "subscribe!"]
-    # msgs = [u"абвгд",u"ежзик",u"лмноп",u"рстуф",u"цчшщъ",u"ыьэюя",u".,!-й"]
-    msgs = [u"Я был бы несчастливейшим из людей,",
-            u"ежели бы я не нашел цели для моей",
-            u"жизни - цели общей и полезной.",
-            u"Теперь же жизнь моя будет вся",
-            u"стремлением деятельным и постоянным",
-            u"к этой одной цели."]
-
-    dx = -300
-    dy = 200
+    #msgs = ["Hello world!", u"Russian: Привет мир", "subscribe!"]
+    #msgs = [u"абвгд",u"ежзик",u"лмноп",u"рстуф",u"цчшщъ",u"ыьэюя",u".,!-й"]
+            
+    msgs = ["abcdef",
+            "ghijkl",
+            "mnopqr",
+            "stuvwx",
+            "yz"]
 
     if len(sys.argv) > 1:
         msg = " ".join(sys.argv[1:])
         msgs = [msg]
 
-    dots = convert(msgs, fontx=15, fonty=15)
+    dots = convert(msgs,fontx=15,fonty=15)
+    dots = transform(dots,[-200,100,0],[0,0,math.pi/3])
 
     myPen = turtle.Turtle()
     myPen.hideturtle()
@@ -31,17 +30,15 @@ def main():
     window.bgcolor("#000000")
     myPen.pensize(2)
     myPen.color("#FF00FF")
-
+    
     for dot in dots:
-        x, y, z = dot
+        x, y, z, a,b,c = dot        
         if z == 0:
             myPen.pendown()
-            myPen.goto(x + dx, y + dy)
         else:
             myPen.penup()
-            myPen.goto(x + dx, y + dy)
-
-
+        myPen.goto(x,y)
+            
 if __name__ == '__main__':
     main()
-    time.sleep(5)
+    time.sleep(3)
